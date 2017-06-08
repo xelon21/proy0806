57a26f7c4dba833b6eda10d838b2a66d7a5740d9
cetecom <cetecom@LC08L1216>
2017-06-08 12:08:57 -0400

    aaaa

--- index.php
+++ index.php
@@ -7,11 +7,24 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <script src="jquery/jquery-3.2.1.min.js" type="text/javascript"></script>
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <form>
+            <input type="text" name="nomusuario" id="nomusuario">
+            <input type="text" name="clave" id="clave">
+            <input id="enviar" type="button" onclick="" value="enviar">
+            
+            
+        </form>
+        
     </body>
+    <script>
+        $(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
+    }); 
+   });
+   </script>
 </html>
